8dfde0206c437ddbc546ea007d716a88c029753d
fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-06-05 17:33:50 -0400

    Update Attempt2.php

--- Attempt2.php
+++ Attempt2.php
@@ -5,74 +5,129 @@
  * Description: A plugin to generate custom letters using GPT API.
 */
 
-function custom_letter_editor_enqueue_scripts() {
-    wp_register_style('custom-letter-editor-style', false);
-    wp_enqueue_style('custom-letter-editor-style');
-    $custom_css = get_option('custom_letter_editor_custom_css');
-    wp_add_inline_style('custom-letter-editor-style', $custom_css);
+<?php
+GFForms::include_addon_framework();
 
-    wp_register_script('custom-letter-editor-script', '', [], '', true);
-    wp_enqueue_script('custom-letter_editor-script');
-    $custom_js = get_option('custom_letter_editor_custom_js');
-    wp_add_inline_script('custom-letter-editor-script', $custom_js);
-}
-add_action('admin_enqueue_scripts', 'custom_letter_editor_enqueue_scripts');
+class GFCustomLetterEditor extends GFAddOn {
+    protected $_version = '1.0';
+    protected $_min_gravityforms_version = '1.9';
+    protected $_slug = 'gf-custom-letter-editor';
+    protected $_path = 'gf-custom-letter-editor/gf-custom-letter-editor.php';
+    protected $_full_path = __FILE__;
+    protected $_url = 'http://www.your-url.com';
+    protected $_title = 'Gravity Forms Custom Letter Editor';
+    protected $_short_title = 'Custom Letter Editor';
+    protected $_requires_credit_card = false;
+    protected $_supports_callbacks = true;
+    
+    private static $_instance = null;
 
-function custom_letter_editor_add_menu_page() {
-    add_menu_page(
-        'Custom Letter Editor Settings',
-        'Custom Letter Editor',
-        'manage_options',
-        'custom-letter-editor',
-        'custom_letter_editor_settings_page_content',
-        'dashicons-email-alt'
-    );
-}
-add_action('admin_menu', 'custom_letter_editor_add_menu_page');
+    public static function get_instance() {
+        if (self::$_instance == null) {
+            self::$_instance = new GFCustomLetterEditor();
+        }
 
-function custom_letter_editor_settings_page() {
-    add_options_page(
-        'Custom Letter Editor',
-        'Custom Letter Editor',
-        'manage_options',
-        'custom-letter-editor',
-        'custom_letter_editor_settings_page_content'
-    );
-}
-add_action('admin_menu', 'custom_letter_editor_settings_page');
-
-function custom_letter_editor_register_widget() {
-    register_widget('CustomLetterEditor');
-}
-add_action('widgets_init', 'custom_letter_editor_register_widget');
-
-// Your existing code for settings page content...
-
-class CustomLetterEditor extends WP_Widget {
-
-    // Your existing code for widget...
-
-    public function update($new_instance, $old_instance) {
-        // Save widget settings
-        $instance = array();
-        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
-        // Repeat for other fields
-        return $instance;
+        return self::$_instance;
     }
+
+    public function plugin_settings_fields() {
+        return array(
+            array(
+                'title'  => 'API Settings',
+                'fields' => array(
+                    array(
+                        'name'    => 'api_key',
+                        'tooltip' => 'Enter your API Key here.',
+                        'label'   => 'API Key',
+                        'type'    => 'text',
+                        'class'   => 'medium',
+                    ),
+                ),
+            ),
+        );
+    }
+
+    public function feed_settings_fields() {
+        return array(
+            array(
+                'fields' => array(
+                    array(
+                        'name'    => 'recipient',
+                        'label'   => 'Recipient',
+                        'type'    => 'text',
+                        'class'   => 'medium',
+                    ),
+                    // More fields as required
+                )
+            )
+        );
+    }
+
+    public function process_feed($feed, $entry, $form) {
+        $api_key = $this->get_plugin_setting('api_key');
+        $recipient = $this->get_field_value($form, $entry, $feed['meta']['recipient']);
+        // More fields as required
+
+        // Use these values to make API call or other processing
+        $result = $this->generate_custom_letter($api_key, $recipient);
+        // Handle $result as required
+    }
+
+    public function generate_custom_letter($apiKey, $recipient) {
+        // Define the API URL
+        $apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+        // Define the headers for the API request
+        $headers = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $apiKey,
+        );
+        // Prepare the prompt using the input parameters
+         $prompt = "Recipient: $recipient\nSubject: $subject\nAdditional Details: $additionalDetails\nName: $name\nEmail: $email\nAddress: $address\n\nSentiment: $selectedSentiment\nWrite a letter:";
+
+        // Define the data for the API request
+        $data = array(
+            'prompt' => $prompt,
+            'max_tokens' => 200, // Adjust the number as needed
+        );
+
+        // Initialize a new cURL session
+        $ch = curl_init();
+
+        // Set the cURL options
+        curl_setopt($ch, CURLOPT_URL, $apiUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Execute the cURL session and fetch the response
+        $response = curl_exec($ch);
+
+        // Close the cURL session
+        curl_close($ch);
+
+        // Decode the response JSON
+        $apiResponse = json_decode($response, true);
+
+        // Check for errors
+        if (isset($apiResponse['choices'][0]['text'])) {
+            $generatedLetter = $apiResponse['choices'][0]['text'];
+            return array(
+                'success' => true,
+                'data' => array('generated_letter' => $generatedLetter),
+            );
+        } else {
+            $errorMessage = 'An error occurred while generating the letter.';
+            if (isset($apiResponse['error']['message'])) {
+                $errorMessage = $apiResponse['error']['message'];
+            }
+            return array(
+                'success' => false,
+                'message' => $errorMessage,
+            );
+        }
+    }
+    // Include other methods like custom_letter_editor_activate(), export_to_csv(), add_export_link() here
 }
 
-function custom_letter_editor_handle_submission() {
-    // Your existing code for form submission...
-}
-
-// Action to handle AJAX request
-add_action('wp_ajax_custom_letter_editor_handle_submission', 'custom_letter_editor_handle_submission');
-add_action('wp_ajax_nopriv_custom_letter_editor_handle_submission', 'custom_letter_editor_handle_submission');
-
-// Your existing code for GPT API...
-
-function custom_letter_editor_activate() {
-    // Your existing code for plugin activation...
-}
-register_activation_hook(__FILE__, 'custom_letter_editor_activate');
-?>
