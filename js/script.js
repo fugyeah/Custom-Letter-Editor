cef1ca94c307548506e655ff93aa1327bbf55acb
fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-06-26 16:07:33 -0400

    Update script.js
    This updated code will display an alert with the generated letter when the request is successful, and an alert with the error message when the request fails. It will also disable the form inputs to prevent multiple submissions.

--- js/script.js
+++ js/script.js
@@ -14,13 +14,21 @@ jQuery(document).ready(function($) {
             data: formData,
             success: function(response) {
                 // Handle the success response
-                console.log(response);
+                if(response.success) {
+                    // Display the generated letter
+                    var generatedLetter = response.data.generated_letter;
+                    alert('Your letter has been generated and sent: ' + generatedLetter);
+                    // Disable the form to prevent multiple submissions
+                    form.find(':input').prop('disabled', true);
+                } else {
+                    // Display the error message
+                    alert(response.data.message);
+                }
             },
             error: function(xhr, status, error) {
                 // Handle the error response
-                console.log(xhr.responseText);
+                alert('An error occurred: ' + xhr.responseText);
             }
         });
     });
 });
-
