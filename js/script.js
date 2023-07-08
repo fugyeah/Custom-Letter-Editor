fce1b9ebee517147edf0b782304f2e095e145ef9
fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-07-08 10:34:18 -0400

    Update Response Message
    I've added a line that displayed a success message with a call to alert(). The alert message includes the response text (generatedLetter), and it will stay on the screen until the user clicks "OK".

--- js/script.js
+++ js/script.js
@@ -1,33 +1,52 @@
-// Add your JavaScript code here
-
 jQuery(document).ready(function($) {
+    var isSubmitting = false; // Flag to check if form is currently being submitted
+
     // AJAX form submission
     $('form').on('submit', function(event) {
         event.preventDefault();
 
+        // If form is currently being submitted, prevent further submissions
+        if (isSubmitting) {
+            console.log('Form is currently being submitted. Please wait...');
+            return;
+        }
+
         var form = $(this);
         var formData = form.serialize();
+
+        // Set flag to true to indicate form is being submitted
+        isSubmitting = true;
 
         $.ajax({
             type: 'POST',
             url: customLetterEditorAjax.ajaxUrl,
             data: formData,
             success: function(response) {
+                // Log the response to the console
+                console.log('AJAX response:', response);
+
                 // Handle the success response
-                if(response.success) {
+                if (response.success) {
                     // Display the generated letter
                     var generatedLetter = response.data.generated_letter;
-                    alert('Your letter has been generated and sent: ' + generatedLetter);
-                    // Disable the form to prevent multiple submissions
-                    form.find(':input').prop('disabled', true);
+                    
+                    // Insert the generated letter into the div
+                    $('#generated-letter').html('<h2>Generated Letter</h2><p>' + generatedLetter + '</p>');
+
+                    // Display a success message with the response text
+                    alert('Form submitted successfully! Response: ' + generatedLetter);
                 } else {
-                    // Display the error message
-                    alert(response.data.message);
+                    // Handle error
+                    alert('An error occurred: ' + response.message);
                 }
             },
             error: function(xhr, status, error) {
                 // Handle the error response
-                alert('An error occurred: ' + xhr.responseText);
+                console.log('AJAX error:', xhr.responseText);
+            },
+            complete: function() {
+                // Set flag to false to allow further submissions
+                isSubmitting = false;
             }
         });
     });
